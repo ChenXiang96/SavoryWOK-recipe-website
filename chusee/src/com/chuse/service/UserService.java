89dea00766d664279284483cdfda03786ae5a50e
ChenXiang96 <982787731@qq.com>
2018-05-22 17:15:56 +0800

    注册的基本实现2

--- chusee/src/com/chuse/service/UserService.java
+++ chusee/src/com/chuse/service/UserService.java
@@ -4,8 +4,15 @@ import com.chuse.entity.User;
 
 public interface UserService {
 
+	
+	public User active(String code);
+	
+	public User findUserByUsernameAndPassword(User user);
+	
 	public User existUser(String userName);
+	
 	public void register(User user);
 	
+	public void update(User user);
 
 }
