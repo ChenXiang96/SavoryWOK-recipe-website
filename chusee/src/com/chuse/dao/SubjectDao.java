a1200ec82c1370bdde4c8d185efca706673d260d
ChenXiang96 <982787731@qq.com>
2018-06-18 19:39:28 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/liuyutaoLYT/-.git
    
    Conflicts:
    	chusee/.classpath
    专题跳转1.0

---
+++ chusee/src/com/chuse/dao/SubjectDao.java
@@ -0,0 +1,19 @@
+package com.chuse.dao;
+
+import java.util.List;
+
+import com.chuse.entity.Product;
+import com.chuse.entity.Subject;
+
+public interface SubjectDao extends BaseDao<Subject>{
+
+	public List<Subject> findHHot();
+	public List<Subject> findNew2();
+	public List<Subject> findByCategorySecondCsid2(Integer casid,Integer page);
+	public List<Subject> findByCategorySecondCategoryCid2(Integer caid,Integer page);
+	public Integer CountPageSubjectFromCategory2(Integer caid);
+	public Integer CountPageSubjectFromCategorySecond2(Integer casid);
+	public Integer CountSubject();
+	public Subject findTwo(Integer pid);
+	public List<Subject> findAll2(Integer page);
+}
---
+++ chusee/src/com/chuse/service/impl/SubjectServiceImpl.java
@@ -0,0 +1,53 @@
+package com.chuse.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.chuse.dao.ProductDao;
+import com.chuse.dao.SubjectDao;
+import com.chuse.entity.Subject;
+import com.chuse.service.ProductService;
+import com.chuse.service.SubjectService;
+
+@Transactional
+@Service("subjectService")
+public class SubjectServiceImpl implements SubjectService {
+
+	@Resource
+	private SubjectDao subjectDao;
+	
+	public Integer CountPageSubjectFromCategory2(Integer caid) {
+		Integer count = subjectDao.CountPageSubjectFromCategory2(caid);
+		return (count % 12 == 0 ? (count / 12) : (count / 12 + 1));
+	}
+	
+	public Integer CountPageSubjectFromCategorySecond2(Integer casid) {
+		Integer count = subjectDao.CountPageSubjectFromCategorySecond2(casid);
+		return (count % 12 == 0 ? (count / 12) : (count / 12 + 1));
+	}
+	public List<Subject> findByCaid(Integer caid, Integer page) {
+		return subjectDao.findByCategorySecondCategoryCid2(caid, page);
+	}
+	public List<Subject> findByCasid(Integer casid, Integer page) {
+		return subjectDao.findByCategorySecondCsid2(casid, page);
+	}
+	public Subject findByPid2(Integer pid) {
+		return subjectDao.findTwo(pid);
+	}
+	public List<Subject> findHHot() {
+		return subjectDao.findHHot();
+	}
+	public List<Subject> findNew2() {
+		return subjectDao.findNew2();
+	}
+
+
+	
+	
+	
+	
+}
